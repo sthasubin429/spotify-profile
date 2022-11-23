adf95b8488fc898bfe11cfc4fc892bf518cc9b7e
subin <subinshrestha429@gmail.com>
2022-11-24 00:10:00 +0545

    feat: add deisng for login and layou

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,8 +3,13 @@ import React, { ReactElement } from 'react';
 
 function Login(): ReactElement {
   return (
-    <div className="min-h-screen grid place-items-center">
-      <Link href="/api/login">Login</Link>
+    <div className="min-h-screen grid place-items-center bg-black">
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold text-white mb-4">Spotify Profile</h1>
+        <Link href="/api/login" className="btn-primary">
+          Login
+        </Link>
+      </div>
     </div>
   );
 }
